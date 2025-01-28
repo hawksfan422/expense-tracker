63b72bdb07aaa285c6da4ff39152770c3ec0bb1f
Steven <steven.acosta867@gmail.com>
2025-01-28 00:44:51 -0500

    Fix

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,6 @@
 import { ChakraProvider } from '@chakra-ui/react'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom' 
 import { AuthProvider } from './context/AuthContext.jsx'
 import App from './App.jsx'
 
