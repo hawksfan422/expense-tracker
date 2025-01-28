3ccd1da5d944bcef583b12df604c9b59fb9d9236
Steven <steven.acosta867@gmail.com>
2025-01-28 00:04:59 -0500

    Deploy to Render p2

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import path from 'path';
 
 import expenseRoutes from './routes/expense.route.js';
 import authRoutes from './routes/auth.route.js'
@@ -11,6 +12,7 @@ dotenv.config();
 
 const app = express();
 
+const __dirname = path.resolve();
 
 app.use(cors());
 app.use(express.json());
@@ -25,6 +27,14 @@ app.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(500).json({ message: 'Something went wrong!' });
   });
+
+if(process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+  app.get("*", (req,res) => {
+      res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
