2fda26f4147ce3a5b85a57e9b253958faac629e0
Steven <steven.acosta867@gmail.com>
2025-01-28 01:00:06 -0500

    Fix

--- backend/middleware/auth.middleware.js
+++ backend/middleware/auth.middleware.js
@@ -1,5 +1,5 @@
 import jwt from 'jsonwebtoken';
-import User from '../models/User.js';
+import User from '../models/user.js';
 import { JWT_SECRET } from '../config/jwt.js';
 import { catchAsync } from '../utils/catchAsync.js';
 
